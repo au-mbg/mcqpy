6ff9de4389292816aa695fccf5e1c5ed0d46e60b
Mads-Peter <machri@phys.au.dk>
2025-11-24 15:33:51 +0100

    Adding accidentally removed files.

---
+++ src/mcqpy/build/latex_questions.py
@@ -0,0 +1,133 @@
+
+from pylatex import (
+    Document,
+    Enumerate,
+    Figure,
+    Section,
+    SubFigure,
+)
+from pylatex.utils import NoEscape
+
+from mcqpy.build.latex_helpers import Form, code_block, multi_checkbox, radio_option
+from mcqpy.question import Question
+from mcqpy.utils.image import check_and_download_tmp
+
+
+def build_question(document: Document, question: Question, quiz_index: int):
+    if question.question_type == "single":
+        extra_section_header = r"Select \underline{one} answer"
+    elif question.question_type == "multiple":
+        extra_section_header = r"Select \underline{all} correct answers"
+    else:
+        extra_section_header = ""
+
+    with document.create(
+        Section(
+            NoEscape(
+                rf"Question {quiz_index + 1} {{\small [{question.point_value} points]}} \hfill {{\small \textit{{{extra_section_header}}}}}"
+            ),
+            numbering=False,
+        )
+    ):
+        document.append(NoEscape(question.text))
+
+        _build_question_image(document, question)
+        _build_question_code(document, question)
+        _build_question_form(document, question, quiz_index)
+
+
+def _build_question_code(document, question: Question):
+    if not question.code:
+        return
+
+    for index, code_snippet in enumerate(question.code):
+        if code_snippet is None:
+            continue
+        language = (
+            question.code_language[index]
+            if index < len(question.code_language)
+            else "python"
+        )
+        document.append(code_block(code_snippet, language))
+
+
+def _build_question_image(document, question: Question):
+    if not question.image:
+        return
+
+    if len(question.image) == 1:
+        image = question.image[0]
+        image = check_and_download_tmp(image, f"tmp_question_{question.qid}_image_0")
+        options = question.image_options.get(0, {}) if question.image_options else {}
+        for key, value in options.items():
+            options[key] = NoEscape(value)
+
+        with document.create(Figure(position="h!")) as fig:
+            fig.add_image(str(image), **options)
+            if question.image_caption and 0 in question.image_caption:
+                fig.add_caption(NoEscape(f"Figure: {question.image_caption[0]}"))
+    else:
+        with document.create(
+            Figure(position="h!"),
+        ) as fig:
+            document.append(NoEscape(r"\centering"))
+            for index, image in enumerate(question.image):
+                options = (
+                    question.image_options.get(index, {})
+                    if question.image_options
+                    else {}
+                )
+
+                image = check_and_download_tmp(
+                    image, f"tmp_question_{question.qid}_image_{index}"
+                )
+
+                newline = options.pop(
+                    "newline", None
+                )  # Remove newline option if present
+                if newline:
+                    fig.append(NoEscape(r"\\"))
+
+                for key, value in options.items():
+                    options[key] = NoEscape(value)
+
+                with document.create(SubFigure(position="b")) as subfig:
+                    subfig.add_image(str(image), **options)
+                    if question.image_caption and index in question.image_caption:
+                        subfig.add_caption(NoEscape(f"{question.image_caption[index]}"))
+
+            if question.image_caption and -1 in question.image_caption:
+                fig.add_caption(NoEscape(f"Figure: {question.image_caption[-1]}"))
+
+
+def _build_question_form(document, question: Question, quiz_index: int):
+    with document.create(Form()):
+        q_slug = question.slug
+        q_qid = question.qid
+
+        with document.create(Enumerate(enumeration_symbol=r"(\alph*)", options={})) as enum:
+            for i, permute_index in enumerate(question.permutation):
+                choice = question.choices[permute_index]
+
+                if question.question_type == "multiple":
+                    command = multi_checkbox(
+                        quiz_index=quiz_index,
+                        q_slug=q_slug,
+                        q_qid=q_qid,
+                        i=i,
+                    )
+
+                elif question.question_type == "single":
+                    command = radio_option(
+                        quiz_index=quiz_index,
+                        q_slug=q_slug,
+                        q_qid=q_qid,
+                        i=i,
+                    )
+
+                enum.add_item(command)
+                enum.append(
+                    NoEscape(
+                        rf"\quad \begin{{minipage}}{{\textwidth}} {choice} \end{{minipage}}"
+                    )
+                )
