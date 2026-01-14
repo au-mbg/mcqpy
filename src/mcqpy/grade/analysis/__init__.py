0980ec08bcddbb789a8c060c99eec70f55cf6bbc
Mads-Peter <machri@phys.au.dk>
2026-01-14 13:52:26 +0100

    Some refactoring of analysis code and two new plots.

---
+++ src/mcqpy/grade/analysis/__init__.py
@@ -0,0 +1,3 @@
+from .overall_analysis import make_quiz_analysis
+from .question_analysis import question_analysis
+from .analysis import QuizAnalysis
--- src/mcqpy/grade/analysis/analysis.py
+++ src/mcqpy/grade/analysis/analysis.py
@@ -1,8 +1,6 @@
 import re
 from pathlib import Path
 
-import matplotlib.pyplot as plt
-import numpy as np
 from pylatex import (
     Command,
     Document,
@@ -20,86 +18,10 @@ from rich.progress import track
 
 from mcqpy.cli.config import GradingConfig
 from mcqpy.compile.preamble import add_preamble
-from mcqpy.grade.utils import GradedQuestion, GradedSet, get_grade_dataframe
+from mcqpy.grade.utils import GradedSet, get_grade_dataframe
 from mcqpy.question import QuestionBank
-
-
-def question_analysis(
-    graded_questions: list[GradedQuestion], out_directory: str | Path = None
-):
-    fig, axes = plt.subplots(1, 2, figsize=(10, 5), layout="constrained")
-
-    fig.suptitle(f"Question Analysis: {graded_questions[0].slug}")
-
-    # Answer distribution:
-    all_onehots = np.vstack([q.student_answers for q in graded_questions])
-    answer_sums = all_onehots.sum(axis=0)
-    answer_labels = [f"{chr(i + 65)}" for i in range(all_onehots.shape[1])]
-
-    correct_answers = graded_questions[0].correct_answers
-
-    colors = [
-        "green" if correct_answers[i] == 1 else "red"
-        for i in range(len(correct_answers))
-    ]
-
-    ax = axes[0]
-    ax.bar(answer_labels, answer_sums, color=colors, alpha=0.7, edgecolor="black")
-    ax.set_xlabel("Answer Options")
-    ax.set_ylabel("Number of Selections")
-    ax.set_title("Distribution of Selected Answers")
-
-    # Score distribution:
-    scores = [q.point_value for q in graded_questions]
-    ax = axes[1]
-    ax.hist(
-        scores,
-        bins=np.arange(-0.5, max(scores) + 1.5, 1),
-        color="blue",
-        alpha=0.7,
-        edgecolor="black",
-    )
-    ax.set_xticks(np.arange(0, max(scores) + 1, 1))
-    ax.set_xlabel("Points Awarded")
-    ax.set_ylabel("Number of Students")
-    ax.set_title("Distribution of Points Awarded")
-
-    # Save figure
-    name = f"{graded_questions[0].slug}.pdf"
-    output_path = Path(out_directory) / name
-
-    plt.savefig(output_path) if out_directory else None
-    plt.close(fig)
-
-    return name
-
-
-def make_quiz_analysis(graded_sets: list[GradedSet], output_dir: str | Path):
-    point_distribution = np.array([gs.points for gs in graded_sets])
-    max_points = graded_sets[0].max_points
-
-    fig, ax = plt.subplots(figsize=(6, 4))
-    ax.hist(
-        point_distribution,
-        bins=np.arange(-0.5, max_points + 1.5, 1),
-        color="blue",
-        alpha=0.7,
-        edgecolor="black",
-    )
-
-    ax.axvline(max_points, color="red", linestyle="dashed", linewidth=1)
-
-    ax.set_xlabel("Total Points")
-    ax.set_ylabel("Number of Students")
-    ax.set_title("Distribution of Total Points")
-
-    name = "quiz_overall_analysis.pdf"
-    output_path = output_dir / name
-
-    plt.savefig(output_path)
-    plt.close()
-    return name
-
+from mcqpy.grade.analysis.question_analysis import question_analysis
+from mcqpy.grade.analysis.overall_analysis import make_quiz_analysis
 
 class QuizAnalysis(Document):
     def __init__(
@@ -152,13 +74,16 @@ class QuizAnalysis(Document):
         self.console.log("Finished generating PDF.")
 
     def build_quiz_analysis(self):
-        name = make_quiz_analysis(self.graded_sets, self.figure_directory)
-        with self.create(Section("Overall Quiz Analysis")):
-            with self.create(Figure(position="h!")) as fig:
-                fig.add_image((Path("figures") / name).as_posix(), width="400px")
-                fig.add_caption("Distribution of Total Points Scored in the Quiz")
+        figures = make_quiz_analysis(self.graded_sets, self.figure_directory)
+        with self.create(Section("Overall Quiz Analysis", numbering="0")):
+            for figure in figures:            
+                with self.create(Figure(position="h!")) as fig:
+                    fig.add_image((Path("figures") / figure.name).as_posix(), width=NoEscape(r"0.8\textwidth"))
+                    fig.add_caption(NoEscape(figure.caption))
 
-            self.append(NewPage())
+        self.append(NoEscape(r"\clearpage"))
+        self.append(NewPage())
+        
 
     def build_question_analyses(self):
         num_questions = len(self.graded_sets[0].graded_questions)
